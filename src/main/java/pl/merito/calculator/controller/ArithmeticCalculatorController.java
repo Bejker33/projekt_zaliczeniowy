2054f24d2df528f1e8faf7868078eda9ac9f92f3
przemyslaw.pyszczek <p.pyszczek@streamsoft.pl>
2025-07-04 21:20:52 +0200

    Create calculator controller and calculator template

---
+++ src/main/java/pl/merito/calculator/controller/ArithmeticCalculatorController.java
@@ -0,0 +1,40 @@
+package pl.merito.calculator.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class ArithmeticCalculatorController {
+
+    @GetMapping("/")
+    public String home(){
+        return "calculator";
+    }
+
+    @GetMapping("/calculate")
+    public String calculator(@RequestParam double a, @RequestParam double b, @RequestParam String operation, Model model) {
+
+        double result = 0.0;
+
+        switch (operation){
+            case "+" -> result = a + b;
+            case "-" -> result = a - b;
+            case "*" -> result = a * b;
+            case "/" -> {
+                if(b == 0) {
+                    model.addAttribute("errorMessage", "Nie można dzielić przez 0");
+                    return "calculator";
+                }
+                result = a / b;
+            }
+        }
+
+        model.addAttribute("result", result);
+
+        return "calculator";
+    }
+}
+
+
